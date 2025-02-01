3947168ec9d2d833e9be7712dacae5b5ddccc566
liandro98 <miguelalejandroespinosa7@gmail.com>
2025-02-01 15:22:24 -0600

    BD local

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'bsnw18tkj2dhvuia4xxe-mysql.services.clever-cloud.com',
-        user: 'ujo3efrckit8pztx',
-        password: 'elsdtQkS7NBWiCjRU3i8',
-        database: 'bsnw18tkj2dhvuia4xxe'
+        host: 'localhost',
+        user: 'root',
+        password: 'm0pq',
+        database: 'BD_ActiPro',
+        port: 3306
     }
 };
